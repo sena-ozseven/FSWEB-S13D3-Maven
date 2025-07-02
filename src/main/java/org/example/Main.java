66a76d9eccb63d71b8b8b37c4042bbd19ba001a7
sena <gulozseven@gmail.com>
2025-07-02 21:25:03 +0300

    Wall Class is done -COMPLETED

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,16 @@ public class Main {
         Person person = new Person("John", "Doe", 20);
 
         System.out.println("Firstname: " + person.getFirstName());
-
         System.out.println("LastName: " + person.getLastName());
-
         System.out.println("Age: " + person.getAge());
 
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
 
     }
 }
